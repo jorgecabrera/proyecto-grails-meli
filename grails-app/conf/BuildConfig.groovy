ed1b16e836ab9365b338d6ecb455f44e7f411d95
Martin Feldsztejn <mfeldsztejn@gmail.com>
2015-03-13 11:33:20 -0300

    configuring spring

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -64,6 +64,7 @@ grails.project.dependency.resolution = {
 		compile ":twitter-bootstrap:3.2.0"
 		compile ":calendar:1.2.1"
 		compile ":mail:1.0.7"
+        compile ":spring-security-core:2.0-RC4"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -85,6 +85,8 @@ grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
 
+grails.plugin.springsecurity.password.algorithm='bcrypt'
+
 environments {
     development {
         grails.logging.jul.usebridge = true
