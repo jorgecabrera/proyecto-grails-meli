b69f9c611d8f02eac7d8fed925bdf45f83dc7195
Martin Feldsztejn <mfeldsztejn@gmail.com>
2015-03-16 09:53:45 -0300

    fixed issue: #1

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,4 @@
-import java.util.Date;
+import java.util.Date
 import usuario.*
 
 class BootStrap {
--- grails-app/domain/usuario/Usuario.groovy
+++ grails-app/domain/usuario/Usuario.groovy
@@ -4,41 +4,30 @@ import regalo.Regalo
 
 class Usuario {
 
-	transient springSecurityService
+	String nombre
+	String apellido
+	String dni
+	Date fechaDeCumpleanios
+	Regalo regaloActual
 
-	String username
-	String password
-	boolean enabled = true
-	boolean accountExpired
-	boolean accountLocked
-	boolean passwordExpired
+	def historialDeRegalos
 
-	static transients = ['springSecurityService']
-
-	static constraints = {
-		username blank: false, unique: true
-		password blank: false
+	def Usuario(String unNombre,String unApellido, Date unaFechaDeCumpleanios,String unDni,String urlRegalo,String urlImagen,float precioRegalo,String idRegalo){
+		nombre=unNombre
+		apellido=unApellido
+		fechaDeCumpleanios=unaFechaDeCumpleanios
+		dni=unDni
+		regaloActual=new Regalo(unaFechaDeCumpleanios,urlRegalo,urlImagen,precioRegalo,idRegalo)
+		historialDeRegalos=new ArrayList<Regalo>()
 	}
 
-	static mapping = {
-		password column: '`password`'
-	}
-
-	Set<Role> getAuthorities() {
-		UsuarioRole.findAllByUsuario(this).collect { it.role }
-	}
-
-	def beforeInsert() {
-		encodePassword()
-	}
-
-	def beforeUpdate() {
-		if (isDirty('password')) {
-			encodePassword()
-		}
-	}
-
-	protected void encodePassword() {
-		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+	def confirmarRegaloActual(){
+		regaloActual.regalado=true
+		historialDeRegalos.add(regaloActual)
+		this.regaloActual=new Regalo(fechaDeCumpleanios,"","",0,"")
+		Date unaFecha=new Date()
+		unaFecha.setYear(unaFecha.getYear()+1)
+		unaFecha.clearTime()
+		regaloActual.setFechaRegalado(unaFecha)
 	}
 }
